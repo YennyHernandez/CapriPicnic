0672a50ded2a2cc59716ae7c01a6b17b09fa58ca
Yenny Hernandez <yeni.hernandez@udea.edu.co>
2020-10-10 17:38:59 -0500

    Se crea estrutura de paquetes junto con las actividades; splash, main, login, register. Se crean animaciones para el splash

---
+++ app/src/main/java/com/yennyh/capripicnic/auth/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.yennyh.capripicnic.auth.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yennyh.capripicnic.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/yennyh/capripicnic/auth/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.yennyh.capripicnic.auth.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yennyh.capripicnic.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
