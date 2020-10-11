68c13e04ee958a0d2595146626e175c13cc85151
Yenny Hernandez <yeni.hernandez@udea.edu.co>
2020-10-10 23:32:08 -0500

    Se crea estrutura de paquetes junto con las actividades; splash, main, login, register. Se crean animaciones para el splash. Se agregan animaciones al splash screen. Se agregan estilos de material y se cambios la patela de colores.

--- app/src/main/java/com/yennyh/capripicnic/SplashActivity.kt
+++ app/src/main/java/com/yennyh/capripicnic/SplashActivity.kt
@@ -1,11 +1,34 @@
 package com.yennyh.capripicnic
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.yennyh.capripicnic.auth.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_splash)
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out) //carga animación
+        logo_imageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.splash_in))  //asincronico, pero con los tiempos de cada uno se sincroniza
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            logo_imageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.splash_out))
+            Handler(Looper.getMainLooper()).postDelayed({
+                logo_imageView.visibility = View.GONE
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }, 500)
+        }, 2500)
     }
 }
