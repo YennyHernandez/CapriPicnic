397bdce2062e1cc53ffaaab198e60d5863547c8f
Jaidiver Gómez <jaidiver.gomez@udea.edu.co>
2020-11-05 00:46:06 -0500

    Se cambia imagenes del splash por unas imagenes vectorizadas, se agrega firebase al proyecto

--- app/src/main/java/com/yennyh/capripicnic/SplashActivity.kt
+++ app/src/main/java/com/yennyh/capripicnic/SplashActivity.kt
@@ -1,10 +1,12 @@
 package com.yennyh.capripicnic
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.view.WindowInsets
 import android.view.WindowManager
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
@@ -14,13 +16,24 @@ import kotlinx.android.synthetic.main.activity_splash.*
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
         setContentView(R.layout.activity_splash)
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out) //carga animación
-        logo_imageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.splash_in))  //asincronico, pero con los tiempos de cada uno se sincroniza
+        logo_imageView.startAnimation(
+            AnimationUtils.loadAnimation(
+                this,
+                R.anim.splash_in
+            )
+        )  //asincronico, pero con los tiempos de cada uno se sincroniza
 
         Handler(Looper.getMainLooper()).postDelayed({
             logo_imageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.splash_out))
@@ -29,6 +42,6 @@ class SplashActivity : AppCompatActivity() {
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
             }, 500)
-        }, 2500)
+        }, 1500)
     }
 }
