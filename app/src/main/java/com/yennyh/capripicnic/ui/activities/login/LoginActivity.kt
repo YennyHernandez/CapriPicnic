10c22f05730153e4f98397fc6920d7827a7951ad
Jaidiver Gómez <jaidiver.gomez@udea.edu.co>
2020-11-28 23:36:21 -0500

    Se eliminan fragmentos inecesarios, se crea main antivity para contener al drawer y bottom navigation, se elimina los anterios arachivos del bottom y de drawer, se cambian íconos de los menus de navegation.

--- app/src/main/java/com/yennyh/capripicnic/ui/activities/login/LoginActivity.kt
+++ app/src/main/java/com/yennyh/capripicnic/ui/activities/login/LoginActivity.kt
@@ -12,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.yennyh.capripicnic.R
-import com.yennyh.capripicnic.ui.activities.navigation.bottom.BottomNavigationActivity
+import com.yennyh.capripicnic.ui.activities.main.MainActivity
 import com.yennyh.capripicnic.ui.activities.passwordrecovery.PasswordRecoveryActivity
 import com.yennyh.capripicnic.ui.activities.register.RegisterActivity
 import kotlinx.android.synthetic.main.activity_login.*
@@ -163,7 +163,7 @@ class LoginActivity : AppCompatActivity() {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    sendHomeData()
+                    sendMainActivity()
                 } else {
                     Toast.makeText(
                         baseContext, "Authentication failed!",
@@ -173,9 +173,9 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
-    private fun sendHomeData() {
-        val drawer = Intent(this, BottomNavigationActivity::class.java)
-        startActivity(drawer)
+    private fun sendMainActivity() {
+        val main = Intent(this, MainActivity::class.java)
+        startActivity(main)
         finish()
     }
 
--- app/src/main/java/com/yennyh/capripicnic/ui/fragments/MyPurchasesFragment.kt
+++ app/src/main/java/com/yennyh/capripicnic/ui/fragments/MyPurchasesFragment.kt
@@ -7,14 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.yennyh.capripicnic.R
 
-class arequipe : Fragment() {
+class MyPurchasesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_arequipe_product, container, false)
+        return inflater.inflate(R.layout.fragment_my_purchases, container, false)
     }
-
 }
